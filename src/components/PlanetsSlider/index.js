3844a4f9a505a61f3efa446a2c58559feee4dc97
JayanthV12 <jayanthvangipuram@gmail.com>
2023-03-11 05:13:31 +0000

    planets

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,26 @@
+// Write your code here
+// Write your code here
+import Slider from 'react-slick'
+
+import './index.css'
+
+import PlanetItem from '../PlanetItem'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    // below comment used to disable eslint for testid line
+    // eslint-disable-next-line react/no-unknown-property
+    <div className="app-container" data-testid="planets">
+      <h1 className="heading">PLANETS</h1>
+      <Slider>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
